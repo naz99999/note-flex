6191804b80e9eeabc73a8823fdef27df45d2bce3
Naman Bindra <68289412+naz99999@users.noreply.github.com>
2024-09-18 18:37:38 +0530

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCAUCKt933Pj81l9IrkPv2vVoIKXmzEKbU",
+    authDomain: "react-notes-f6d8d.firebaseapp.com",
+    projectId: "react-notes-f6d8d",
+    storageBucket: "react-notes-f6d8d.appspot.com",
+    messagingSenderId: "1031543116191",
+    appId: "1:1031543116191:web:665e95fa8cea32de922daa"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+export const notesCollection = collection(db, "notes")
